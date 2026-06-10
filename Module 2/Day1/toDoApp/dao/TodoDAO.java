dbd6ba40b4a2cfbaba7b91ae3360f99ce1b2abab
Sangram Sethi | NorthernArc <sangram.sethi@NorthernArc.com>
2026-06-10 07:20:02 +0530

    Created the todoDAO interface

---
+++ Module 2/Day1/toDoApp/dao/TodoDAO.java
@@ -0,0 +1,17 @@
+package Day1.toDoApp.dao;
+import Day1.toDoApp.model.Todo;
+import java.util.List;
+
+public interface TodoDAO {
+
+    void addTodo(Todo todo);
+
+    Todo findById(int id);
+
+    List<Todo> findAll();
+
+    void updateTodo(Todo todo);
+
+    void deleteTodo(int id);
+    
+}
